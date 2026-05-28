a99a5e88846777972de5d99e28d34bb8609a5f18
Aya Abed <52230512@students.liu.edu.lb>
2026-05-28 20:51:10 +0300

    Add AppDbContext class inside the Data Folder

---
+++ DoctorsAppointmentsProject_Phase2/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorsAppointmentsProject_Phase2.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppDbContext : ControllerBase
+    {
+    }
+}
